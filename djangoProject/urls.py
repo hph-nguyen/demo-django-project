3640036279f425321cfd0f91411df6a607ed9e1f
hph-nguyen <hph.nguyen96@gmail.com>
2020-12-03 20:36:37 +0100

    updated

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from posts import views
 
 urlpatterns = [
-    path('',include('homepage.urls')),
+    path('',views.index,name="posts"),
     path('admin/', admin.site.urls),
     path("polls/",include("polls.urls")),
     path('posts/',include("posts.urls")),
